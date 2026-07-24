29e8324e8cb69cd8c17a211d0322e463713b3c92
Javielito1965 <gotojordan@gmail.com>
2026-07-24 10:11:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // CONFIGURACIÓN — rellena estos dos valores con los de tu proyecto Supabase
 // Los encuentras en: Supabase > Project Settings > API
 // =========================================================
-const SUPABASE_URL = 'https://xrwflhncyivyxpvpveuh.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://xrwflhncyivyxpvpveuh.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_7B-_cxXFl_IbYaxLaoaOrA_CAbMQqbl';
